f7f5eff735f39061eab13f2faeb867e34382cb67
blaserlab <bradley.blaser@osumc.edu>
2023-05-18 22:42:58 -0400

    added alternative installation code to dependencies.R

--- R/dependencies.R
+++ R/dependencies.R
@@ -1,5 +1,3 @@
-# blaseRtemplates::easy_install("blaserlab/blaseRtools", "link_from_cache")
-
 
 suppressPackageStartupMessages(library("conflicted"))
 suppressPackageStartupMessages(library("blaseRtools"))
@@ -28,4 +26,12 @@ suppressPackageStartupMessages(library("pander"))
 suppressPackageStartupMessages(library("Cairo"))
 
 
+# load, install, and/or update the project data -----------------------------
+# this requires that you are using a blaseRtemplates installation, see https://blaserlab.github.io/blaseRtemplates/articles/establish.html
+# you would have to edit the path variable to point to the directory holding the pkc.cxcl8.datapkg tar.gz file
 blaseRtemplates::project_data("/home/OSUMC.EDU/blas02/network/X/Labs/Blaser/Brad/projects/pkc_cxcl8_data/datapkg")
+
+# alternatively, use the data function to load the data objects after installing pkc.cxcl8.datapkg
+# d <- data(package = "pkc.cxcl8.datapkg")
+# data(list = d$results[, "Item"], package = "pkc.cxcl8.datapkg")
+# rm(d)
